24a683f475f236330c946338bdd8c3dd17bbcd6a
kyu <kyumfg@gmail.com>
2014-09-20 15:40:33 +0900

    Update NP_ChgSkin2SP.php
    v1.3

--- NP_ChgSkin2SP.php
+++ NP_ChgSkin2SP.php
@@ -1,26 +1,28 @@
 <?php
-/* NP_ChgSkin2SP v1.1
+/* NP_ChgSkin2SP v1.3
  * 
  * UserAgentによりスマートホンを判別し、適切なSkinへ振り分け
  * Nakazoe氏(nakazoe@comiu.com)作 NP_AdjustSkin2MobileLite 0.2を改造
  * 
  * v1.0 2013.09.21 初版 kyu
  * v1.1 2014.05.08 Feed(RSS/ATOM)パースエラー修正  kyu
+ * v1.2 2014.09.14 細かな修正 yama, kyu
+ * v1.3 2014.09.20 プラグインオプション設定でスキンの選択をselectに変更 kyu
  */
 
 class NP_ChgSkin2SP extends NucleusPlugin{
     function getName()       {return 'ChgSkin2SP';}
     function getAuthor()     {return 'kyu';}
-    function getVersion()    {return '1.1[2014.05.08]';}
+    function getVersion()    {return '1.3[2014.09.20]';}
     function getURL()        {return 'mailto:kyumfg@gmail.com';}
     function getDescription(){return 'UserAgentによりスマートホンを判別し、適切なSkinへ振り分けます。スキンに<%ChgSkin2SP%>と記述するとPC表示/スマートホン表示を切り替えるためのリンクを出力します。';}
     
     function supportsFeature($w) {return in_array($w, array('SqlTablePrefix'));}
     function getMinNucleusVersion(){return '341';}
-    function getEventList()  {return array('InitSkinParse');}
+    function getEventList()  {return array('InitSkinParse','PostAddSkin','PostDeleteSkin');}
     
     function install() {
-        $this->createOption('spskinname','スマートホン表示で使用するスキン名','text','smartphone');
+        $this->createOption('spskinname','スマートホン表示で使用するスキン名','select',$this->DefaultSkin(),$this->SkinList());
         $this->createOption('viewsp','スキン変数<%ChgSkin2SP%>: スマートホン表示するためのリンク名','text','スマートホン表示');
         $this->createOption('viewpc','スキン変数<%ChgSkin2SP%>: PC表示するためのリンク名','text','PC表示');
     }
@@ -31,30 +33,41 @@ class NP_ChgSkin2SP extends NucleusPlugin{
         $this->deleteOption('viewpc');
     }
     
-    //スキンパース前処理
+    function event_PostAddSkin($data) {
+        $this->UpdateExtra($data);
+    }
+    
+    function event_PostDeleteSkin($data) {
+        $this->UpdateExtra($data);
+    }
+    
     function event_InitSkinParse(&$data){
         if (!$this->isSmartPhone()) return;
         $request_uri = $_SERVER['REQUEST_URI'];
         if (strpos($request_uri, '.php') !== false && strpos($request_uri, 'index.php') === false)
             return;
-
+        
         $viewmode = getVar('viewmode');
         if (is_null($viewmode))
             $viewmode = cookieVar('viewmode');
+            if  (is_null($viewmode))
+                $viewmode = 1;
+            
         elseif (is_null($viewmode) == false)
             $viewmode = intval($viewmode);
         
-        if ($viewmode == 0 || $viewmode == 1){
+        if ($viewmode == 0 || $viewmode == 1)
             setcookie('viewmode', $viewmode);
-        }
-        if ($viewmode == 1 || is_null($viewmode))
+        
+        if ($viewmode == 1)
         {
-                $optionSpskinname = htmlspecialchars($this->getOption('spskinname'), ENT_QUOTES, _CHARSET);
-                
-                if(!SKIN::exists($optionSpskinname))
-                    $SkinName = $data['skin']->name;
-                else
-                    $SkinName = $optionSpskinname;
+            $optionSpskinname = htmlspecialchars($this->getOption('spskinname'), ENT_QUOTES, _CHARSET);
+            
+            if(!SKIN::exists($optionSpskinname))
+                $SkinName = $data['skin']->name;
+             else
+                $SkinName = $optionSpskinname;
+            
         }
         elseif ($viewmode == 0)
             $SkinName = $data['skin']->name;
@@ -63,23 +76,22 @@ class NP_ChgSkin2SP extends NucleusPlugin{
         
         $skin =& SKIN::createFromName($SkinName);
         $data['skin']->SKIN($skin->getID());
-    
         return;
     }
     
-    //スキン変数
     function doSkinVar(){
         if (!$this->isSmartPhone()) return;
         $viewmode = getVar('viewmode');
-        if (is_null($viewmode)){
+        if (is_null($viewmode))
             $viewmode = cookieVar('viewmode');
-        }
-        if (is_null($viewmode)){
+        
+        if (is_null($viewmode))
+        {
             if ($this->isSmartPhone()) $viewmode = 1;
             else                       $viewmode = 0;
         }
         else                           $viewmode = intval($viewmode);
-    
+        
         $Url = 'http://'. $_SERVER['HTTP_HOST']. $_SERVER['REQUEST_URI'];
         
         if (strpos($Url,'?') === false) $Url .= '?';
@@ -92,15 +104,15 @@ class NP_ChgSkin2SP extends NucleusPlugin{
             $optionViewpc = htmlspecialchars($this->getOption('viewpc'), ENT_QUOTES, _CHARSET);
             $echo = '<div class="viewmode">';
             if ($viewmode == 0)
-                $echo .= sprintf('<a href="%s">%s</a>', "{$Url}viewmode=1", $optionViewsp);
+               $echo .= sprintf('<a href="%s">%s</a>', "{$Url}viewmode=1", $optionViewsp);
             elseif ($viewmode == 1)
                 $echo .= sprintf('<a href="%s">%s</a>', "{$Url}viewmode=0", $optionViewpc);
+            
             $echo .= '</div>';
             echo $echo;
         }
     }
     
-    //UA判定
     function Platform(){
         $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
         
@@ -138,4 +150,32 @@ class NP_ChgSkin2SP extends NucleusPlugin{
         else
             return false;
     }
+    
+    function UpdateExtra($data){
+        $sql = "SELECT `oid` FROM `nucleus_plugin_option_desc` WHERE `opid`='".$this->getID()."' AND `oname`='spskinname'";
+        $res = mysql_fetch_assoc(sql_query($sql));
+        $sql = "UPDATE `nucleus_plugin_option_desc` SET `odef`='".$this->DefaultSkin()."',`oextra`='".$this->SkinList()."' WHERE `oid`='".$res['oid']."'";
+        sql_query($sql);
+    }
+    
+    function SkinList(){
+        $res = sql_query("SELECT sdname FROM `nucleus_skin_desc` ORDER BY `nucleus_skin_desc`.`sdname` ASC ");
+        while ($_ = mysql_fetch_assoc($res)) {
+            $extra .= $_['sdname']."|".$_['sdname']."|";
+        }
+        $extra = substr($extra,0,strlen($extra)-1);
+        return $extra;
+    }
+    
+    function DefaultSkin(){
+        $_ = htmlspecialchars($this->getOption('spskinname'), ENT_QUOTES, _CHARSET);
+        if (empty($_)){
+            $sql = "SELECT sdname FROM `nucleus_skin_desc` ORDER BY `nucleus_skin_desc`.`sdname` ASC ";
+            $res = mysql_fetch_assoc(sql_query($sql));
+            $_ = $res['sdname'];
+        }
+        $this->setOption('spskinname',$_);
+        return $_;
+    }
 }
+?>
